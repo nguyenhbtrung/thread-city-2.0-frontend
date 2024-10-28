4fec822bf9e0c2dba59db7495c97a93b35bbe9c8
Đỗ Đăng Huy <sieunhan482@gmail.com>
2024-10-28 18:10:19 +0700

    add sign-in view template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SignIn from './views/sign-in/SignIn.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <SignIn />
     </div>
   );
 }
