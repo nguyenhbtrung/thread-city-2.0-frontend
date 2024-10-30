38569c4b5d9094434b2d758fd320f309beb4a4a2
Setsuna2207 <setsuna2207.yae@gmail.com>
2024-10-30 23:08:22 +0700

    dcm xóa nhầm :))

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import SignIn from './views/sign-in/SignIn';
 import SignUp from './views/sign-up/SignUp';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './Components/NavBar/NavBar.js';
 import Home from './views/home.js';
 import Search from './views/search.js';
-import Post from  './views/post.js';
+import Post from './views/post.js';
 import Favorite from './views/favorite.js';
 import Profiles from './views/favorite.js';
 import Pin from './views/pin.js';
 import Menu from './views/menu.js';
 
-
 function App() {
+
   return (
     <div className="App">
       <Router>
         <div>
-          <NavBar/>
+          <ConditionalNavBar />
         </div>
         <Routes>
           <Route path="/" element={<SignUp />} />
@@ -36,5 +36,14 @@ function App() {
     </div>
   );
 }
+
+const ConditionalNavBar = () => {
+  const location = useLocation();
+
+  // Ẩn thanh NavBar ở trang đăng kí/ đăng nhập
+  const hideNavBarPaths = ['/sign-in', '/sign-up', '/'];
+
+  return !hideNavBarPaths.includes(location.pathname) ? <NavBar /> : null;
+};
 
 export default App;
