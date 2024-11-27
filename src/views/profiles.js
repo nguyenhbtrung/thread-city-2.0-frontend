973626af02c7acfb95e4fdcfe7507ab93ce63c7e
Đỗ Đăng Huy <sieunhan482@gmail.com>
2024-11-27 10:34:38 +0700

    fix profile page

--- src/views/profiles.js
+++ src/views/profiles.js
@@ -18,6 +18,7 @@ const Profiles = () => {
     useEffect(() => {
         console.log(userName);
         getProfileData();
+        getProfilePost();
     }, [userName]);
 
     const getProfileData = async () => {
@@ -52,57 +53,58 @@ const Profiles = () => {
         } catch (error) {
             toast.error("Có lỗi xảy ra");
         }
-}
-
-const handleScroll = () => {
-    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-
-    if (scrollTop + clientHeight >= scrollHeight) {
-        setPage(prev => prev + 1);
     }
-};
 
-useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    const handleScroll = () => {
+        const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
 
-    return () => window.removeEventListener("scroll", handleScroll);
-}, []);
-
-useEffect(() => {
-    if (page > 1) {
-        LoadMorePost();
-    }
-}, [page]);
-
-const LoadMorePost = async () => {
-    try {
-        const post = await GetProfilePosts(userName, page);
-        if (post.status === 200) {
-            setPosts(prev => [...prev, ...post.data]);
+        if (scrollTop + clientHeight >= scrollHeight) {
+            setPage(prev => prev + 1);
         }
-    } catch (error) {
-        toast.error("Có lỗi xảy ra");
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    useEffect(() => {
+        if (page > 1) {
+            LoadMorePost();
+        }
+    }, [page]);
+
+    const LoadMorePost = async () => {
+        try {
+            const post = await GetProfilePosts(userName, page);
+            if (post.status === 200) {
+                setPosts(prev => [...prev, ...post.data]);
+            }
+        } catch (error) {
+            toast.error("Có lỗi xảy ra");
+        }
+    };
+
+    const ResetPosts = () => {
+        setPage(1);
+        setPosts([]);
+        getProfileData();
+        getProfilePost();
     }
-};
 
-const ResetPosts = () => {
-    setPage(1);
-    setPosts([]);
-    getProfileData();
-}
-
-return (
-    <div style={{ marginTop: '20px' }}>
-        <div style={{ margin: 0, display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-            {profileData && <ProfileInfo{...profileData} />}
+    return (
+        <div style={{ marginTop: '20px' }}>
+            <div style={{ margin: 0, display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                {profileData && <ProfileInfo{...profileData} />}
+            </div>
+            <PostList
+                posts={posts}
+                loading={loadingPost}
+                onDeletedSuccessfully={ResetPosts}
+            />
         </div>
-        <PostList
-            posts={posts}
-            loading={loadingPost}
-            onDeletedSuccessfully={ResetPosts}
-        />
-    </div>
-)
+    )
 }
 
 export default Profiles
